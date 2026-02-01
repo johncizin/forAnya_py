47b9221390207984f04152121dbe8c3d89ad9a56
John Cizin <68138359+SoaringAbove@users.noreply.github.com>
2026-02-01 01:16:39 -0500

    added writing to google sheets w dynamic vars and static from retell webhook call_analyzed packet works soundly

--- src/webHook/server.js
+++ src/webHook/server.js
@@ -6,29 +6,71 @@ const { google } = require('googleapis');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SHEET_ID = process.env.SHEET_ID;
 
 const auth = new google.auth.GoogleAuth({
     keyFile: process.env.GOOGLE_SERVICE_ACCOUNT_JSON,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
+
+const sheet = google.sheets({ version: 'v4', auth });
 
 app.use(bodyParser.json());
 
 //post endpoint to receive webhook events
 // this receives call ended events from retell.ai
 app.post('/', async (req, res) => {
-    if (req.body && req.body.event === 'call_ended') {
-        console.log('Call ended event received');
-         console.log('Received webhook payload:', req.body.call.transcript);
+
+    try{
+        if(!req.body || req.body.event !== 'call_analyzed'){
+            return res.status(400).send('Invalid event type');
+        }
+
+        const call = req.body.call;
+        //console.log(req.body)
+        const callId = call?.call_id;
+        const firstName = call?.collected_dynamic_variables?.first_name;
+        const timeStamp = new Date().toString();
+        const transcript = call?.transcript || '';
+        const disonnectedReason = call?.disconnection_reason || '';
+
+        console.log(transcript);
+
+        await sheet.spreadsheets.values.append({
+            spreadsheetId: process.env.SHEET_ID,
+            range: 'Sheet1!A:E',
+            valueInputOption: 'USER-ENTERED',
+            requestBody: {
+                values: [[
+                    callId,
+                    firstName,
+                    timeStamp,
+                    transcript,
+                    disonnectedReason
+                ]]
+            }
+        });
+        console.log('written')
+         res.status(200).send('Received and processed');
+    } catch(err){
+        console.error("ERROR:")
+        console.error(err.message);
+    } 
+    //for testing purposes
+    /*
+    if (req.body) {
+       // console.log('Call ended event received');
+         console.log(req.body.call);
          res.status(200).send('Webhook received');
     } else {
         res.status(400).send('Invalid event type');
-    }
+    } */
 });
 
 //get endpoint to fetch data from Google Sheets
 //just tryingto get the data from the spreadsheet rn as a test
-//TODO: implement
+//Ight this works now gonna do the actual implementation
+/*
 app.get('/read-sheet-data', async (req, res) => {
     try{
         const sheet = google.sheets({ version: 'v4', auth });
@@ -52,7 +94,8 @@ app.get('/read-sheet-data', async (req, res) => {
     }
     res.status(500).json({ error: 'Failed to read sheet' });
   }
-});
+}); */
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
